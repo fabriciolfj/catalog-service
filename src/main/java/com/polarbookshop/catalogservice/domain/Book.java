4bbf02ecfc43ede21dd03996774fdabbc3f57cd9
spark <fabricio.jacob@outlook.com>
2022-05-24 23:42:45 -0300

    add

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -7,10 +7,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.annotation.Version;
+import org.springframework.data.annotation.*;
 
 public record Book(
         @Id
@@ -30,11 +27,15 @@ public record Book(
         Instant createdDate,
         @LastModifiedDate
         Instant lastModifiedDate,
+        @CreatedBy
+        String createdBy,
+        @LastModifiedBy
+        String lastModifiedBy,
         @Version
         int version) {
 
     public static Book build(String isbn, String title, String author, Double price, String publisher) {
-        return new Book(null, isbn, title, author, price, publisher, null, null, 0);
+        return new Book(null, isbn, title, author, price, publisher, null, null, null, null, 0);
     }
 
 }
